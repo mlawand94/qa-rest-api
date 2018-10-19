7b95fb5aa079a283e4f1be6e0a8166dd1a3e0e0a
mlawand94 <mlawand94@gmail.com>
2018-10-18 22:35:31 -0700

    Adding jsonCheck with bodyParser

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 
 var express = require('express');
 var app = express();
+var jsonParser = require("body-parser").json;
+
+var jsonCheck = function(req,res,next){
+    if(req.body){
+        console.log("The sky is ", req.body.color);
+    }else{
+        console.log("There is no body property on the request");
+    }
+    next();
+}
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
+
 
 var port = process.env.PORT || 3001;
 
