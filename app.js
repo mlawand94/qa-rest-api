fd2ca08d8c543c067c71e568650ca9ec3b08e162
mlawand94 <mlawand94@gmail.com>
2018-10-23 22:16:35 -0700

    CORS Configuration

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ db.once("open", function(){
     console.log("db connection successful");
 });
 
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use("/questions", routes);
 
